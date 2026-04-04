4365e1dd16c2901ff91745bc4dbdf7d569d2519d
Gee <alickoamarko@gmail.com>
2026-04-04 09:15:05 +0200

    WIP: Linux implementation (FLTK)

--- linux/main.cpp
+++ linux/main.cpp
@@ -2,54 +2,57 @@
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
+#include <vector>
 
 #include <linux/limits.h>
 #include <unistd.h>
 
 #include <FL/Fl.H>
+#include <FL/fl_draw.H>
 #include <FL/Fl_Window.H>
+#include <FL/Fl_Button.H>
 
 
-// Print formatted message to stderr with filename, line, and function name
 #define ERROR(fmt, ...) \
-        do { fprintf( \
-                stderr, \
+        do { Fl::fatal( \
                 "[ERROR] %s@%d (%s): " fmt "\n", \
                         __FILE__, __LINE__, __func__, \
                         ##__VA_ARGS__ \
         ); } while (0)
 
 
-// int _OfferChoice(const char** choices, int num_of_choices) {
-//         for (int i = 1; i < num_of_choices; i++) {
-//                 printf("%d) %s", i, choices[i-1]);
-//         }
-//         // TODO
-// }
-// #define OfferChoice(...) \
-//         _OfferChoice( \
-//                 (const char*[]){__VA_ARGS__}, \
-//                 sizeof((const char*[]){__VA_ARGS__}) / sizeof(const char*) \
-//         )
+char cmd[ARG_MAX];
+int cmd_len = 0;
+
+void ExecuteTarget(const char* target_path, const char* input_fstring) {
+        cmd_len = strlen(target_path);
+        memcpy(cmd, target_path, cmd_len);
+        cmd[cmd_len++] = ' '; cmd[cmd_len] = '\0';
+
+        // TODO: Parse input_fstring -> gather inputs -> append to cmd
+
+        puts(cmd); puts(input_fstring); exit(0); // TEMP; TEST
+        //exit(system(cmd));
+}
+
+typedef struct {
+        char* target_path;
+        char* label;
+        char* input_fstring;
+} Button;
 
 char initial_working_directory[PATH_MAX];
 
 int main(int argc, char* argv[]) {
-        // TEMP; TEST:
-        Fl_Window* window = new Fl_Window(340, 180);
-        window->end();
-        window->show();
-        return Fl::run();
+        ERROR("TEST - %s - %d", "wad", 727);
 
-        if (argc < 4) {
-                puts("USAGE: <<PATH/TO/TARGET> <BUTTON_LABEL> <INPUT_FORMAT>> ...");
-                return 1;
-        }
+        const int font_height = Fl::h() / 45;
 
-        if (!getcwd(initial_working_directory, PATH_MAX)) {
-                ERROR("Failed to get current working directory");
-                return 1;
-        }
+        fl_font(FL_HELVETICA, font_height);
+
+        if (argc < 4) ERROR("USAGE: <<PATH/TO/TARGET> <BUTTON_LABEL> <INPUT_FORMAT>> ...");
+
+        if (!getcwd(initial_working_directory, PATH_MAX)) ERROR("Failed to get current working directory");
         atexit([](){
                 if (!chdir(initial_working_directory)) {
                         ERROR(
@@ -61,14 +64,57 @@ int main(int argc, char* argv[]) {
 
         char* path_sep = strrchr(argv[0], '/');
         if (path_sep) *path_sep = '\0';
-        if (!chdir(argv[0])) {
-                ERROR("Failed to change woring directory to '%s'", argv[0]);
-                return 1;
+        if (!chdir(argv[0])) ERROR("Failed to change working directory to '%s'", argv[0]);
+
+
+        // Buttonless mode
+        if ((strlen(argv[2]) == 1) && (argv[2][0] == ' ')) ExecuteTarget(argv[1], argv[3]);
+
+
+        std::vector<Button> buttons;
+        for (int i = 1; i < argc;) {
+                char* target_path = argv[i++];
+                if (i == argc) Fl::fatal("Target '%s' is missing a label", target_path);
+                char* button_label = argv[i++];
+                if (i == argc) Fl::fatal("Target '%s' with label '%s' is missing an input format", target_path, button_label);
+                char* input_format = argv[i++];
+
+                buttons.push_back(Button{target_path, button_label, input_format});
         }
 
+        int largest_label_width = 0;
+        for (Button& button : buttons) {
+                int w, _h;
+                fl_measure(button.label, w, _h);
+                if (w > largest_label_width) largest_label_width = w;
+        }
 
-        // TODO
+        int button_height = font_height;
 
+        Fl_Window* window = new Fl_Window(
+                largest_label_width,
+                button_height * buttons.size()
+        );
 
-        return 0;
+        for (int i = 0; i < buttons.size(); i++) {
+                Fl_Button* button = new Fl_Button(
+                        0,
+                        button_height * i,
+                        largest_label_width,
+                        button_height,
+                        buttons[i].label
+                );
+                button->user_data(&buttons[i]);
+                button->callback([](Fl_Widget*, void* user_data){
+                        Button* _button = (Button*)user_data;
+                        ExecuteTarget(
+                                _button->target_path,
+                                _button->input_fstring
+                        );
+                });
+        }
+
+        window->end();
+        window->show();
+        return Fl::run();
 }
