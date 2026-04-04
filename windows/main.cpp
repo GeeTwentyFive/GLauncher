3304aa31aa71ec67f5bc9a4def484ea93d84cea1
Gee <alickoamarko@gmail.com>
2026-04-04 15:17:39 -0700

    WIP: Windows implementation

--- windows/main.cpp
+++ windows/main.cpp
@@ -1,6 +1,10 @@
+#define UNICODE
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <shellapi.h>
+
+
+// TODO: #define ERROR(fmt, ...)
 
 
 int WINAPI wWinMain(
@@ -12,7 +16,41 @@ int WINAPI wWinMain(
         int argc;
         LPWSTR* argv = CommandLineToArgvW(GetCommandLineW(), &argc);
 
-        // TODO
+        RegisterClassExW(&(WNDCLASSEXW){
+                .cbSize = sizeof(WNDCLASSEXW),
+                .lpfnWndProc = [](
+                        HWND hWnd,
+                        UINT uMsg,
+                        WPARAM wParam,
+                        LPARAM lParam
+                ) -> LRESULT {
+                        switch (uMsg) {
+                                // TODO
+
+                                case WM_DESTROY:
+                                        PostQuitMessage(0);
+                                        return 0;
+                        }
+
+                        return DefWindowProcW(hWnd, uMsg, wParam, lParam);
+                },
+                .hInstance = hInstance,
+                .hbrBackground = (HBRUSH)(COLOR_WINDOW + 1),
+                .lpszClassName = L"MainWindowClass"
+        });
+
+        HWND hWnd = CreateWindowExW(
+                // TODO
+        );
+        if (!hWnd) { } // TODO: ERROR()
+
+        ShowWindow(hWnd, nCmdShow);
+
+        MSG msg = {};
+        while (GetMessageW(&msg, NULL, 0, 0) > 0) {
+                TranslateMessage(&msg);
+                DispatchMessageW(&msg);
+        }
 
         return 0;
 }
