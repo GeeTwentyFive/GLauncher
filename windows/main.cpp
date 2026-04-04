878d25c623664c7667d49b3ac8ea5efad9495c34
Gee <alickoamarko@gmail.com>
2026-04-04 14:44:45 -0700

    WIP: Windows implementation foundation

--- windows/main.cpp
+++ windows/main.cpp
@@ -1,1 +1,18 @@
-// TODO
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <shellapi.h>
+
+
+int WINAPI wWinMain(
+        HINSTANCE hInstance,
+        HINSTANCE hPrevInstance,
+        PWSTR pCmdLine,
+        int nCmdShow
+) {
+        int argc;
+        LPWSTR* argv = CommandLineToArgvW(GetCommandLineW(), &argc);
+
+        // TODO
+
+        return 0;
+}
